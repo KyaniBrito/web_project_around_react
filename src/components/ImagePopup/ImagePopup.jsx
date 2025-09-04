1a09e2907cde80f5eb34ca3ce78cc1c83994ee68
KyaniBrito <kyanimag@icloud.com>
2025-09-04 11:31:16 -0300

    corrige fechamento do popup

--- src/components/ImagePopup/ImagePopup.jsx
+++ src/components/ImagePopup/ImagePopup.jsx
@@ -1,16 +1,10 @@
 export default function ImagePopup(props) {
   const { card } = props;
 
-  console.log(card);
   return (
     <div className="popup__image-container">
       <img className="popup__image" src={card.link} alt={card.name} />
       <p className="popup__caption ">{card.name}</p>
-      <button
-        className="popup__close-button"
-        aria-label="Fechar imagem"
-        type="button"
-      ></button>
     </div>
   );
 }
